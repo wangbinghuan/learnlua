5daff39b37dc043cddce4bc611ae71a7a566c1e5
WANG <wang@MyServer.wang.com>
2014-03-13 22:41:12 +0800

    hello, lua

---
+++ Hello/src/main.lua
@@ -0,0 +1,16 @@
+--print "Hello"
+
+--function max(a, b)
+--	if a>b then
+--		return a
+--	else
+--		return b
+--	end
+--end
+--
+--print (max(2, 7))
+--
+--for i=1, 10 do
+--	print("hello "..i)
+--end
+--
