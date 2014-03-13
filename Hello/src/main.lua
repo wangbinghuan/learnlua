87ff9d389295da04e7739f70b34f266bd595b70c
WANG <wang@MyServer.wang.com>
2014-03-13 23:36:06 +0800

    syntax demos

--- Hello/src/main.lua
+++ Hello/src/main.lua
@@ -15,5 +15,36 @@
 --end
 --
 
-num=1.3
-print(num*2)
+--num=1.3
+--print(num*2)
+--
+--arr = {"string", 100, "haoel", function() print("coolshell.cn") end}
+--for i=1, #arr do
+--  print (arr[i])
+--end
+	
+--function myPower(x)
+--    return function(y) return y^x end
+--end
+-- 
+--power2 = myPower(2)
+--power3 = myPower(3)
+-- 
+--print(power2(4)) --4的2次方
+--print(power3(5)) --5的3次方
+
+
+--function getUserInfo(id)
+--    print(id)
+--    return "haoel", 37, "haoel@hotmail.com", "http://coolshell.cn"
+--end
+-- 
+--name, age, email, website, bGay = getUserInfo("1")
+--
+--print (bGay)
+
+pi=3.14
+print (_G.pi)
+
+
+
